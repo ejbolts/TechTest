9360671da58506dd822ed8fe3f6c06b6a7f9a7e3
Ethan <ejbolts@hotmail.com>
2024-10-20 01:27:39 +1000

    Refactor program.cs to add controller support and serve static files

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
-app.MapGet("/", () => "Hello World!");
-
+//app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 app.Run();
